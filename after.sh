82f94259296d67e40e3cc9d360713960a29cc7fd
Dirk Hohndel <dirk@hohndel.org>
2017-11-14 07:42:08 -0800

    use correct repo
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- after.sh
+++ after.sh
@@ -3,6 +3,6 @@
 RELEASE_NAME="continuous"
 RELEASE_TITLE="Release for continuous"
 BODY="Testin 1-2-3"
-release_id=$(curl https://api.github.com/repos/Subsurface-divelog/subsurface/releases/tags/${RELEASE_NAME} | grep id: |grep "\"id\":" | head -n 1 | tr -s " " | cut -f 3 -d" " | cut -f 1 -d ",")
+release_id=$(curl https://api.github.com/repos/dirkhh/testrepo/releases/tags/${RELEASE_NAME} | grep id: |grep "\"id\":" | head -n 1 | tr -s " " | cut -f 3 -d" " | cut -f 1 -d ",")
 release_infos=$(curl -H "Authorization: token ${GITHUB_TOKEN}" --request PATCH \
-       --data '{"tag_name": "'"$RELEASE_NAME"'","name": "'"$RELEASE_TITLE"'","body": "'"$BODY"'"}' "https://api.github.com/repos/Subsurface-divelog/subsurface/releases/${release_id}")
+       --data '{"tag_name": "'"$RELEASE_NAME"'","name": "'"$RELEASE_TITLE"'","body": "'"$BODY"'"}' "https://api.github.com/repos/dirkhh/testrepo/releases/${release_id}")
